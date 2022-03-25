d28c34cda64fc9d3a898c2a9472ca1ada22f668c
LisaVC <lisaviju@gmail.com>
2022-03-26 02:29:09 +0530

    added editing option

--- grocery-list/src/App.js
+++ grocery-list/src/App.js
@@ -8,31 +8,66 @@ function App() {
   const [list, setList] = useState([]);
   const [editing, setEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: true, msg: '', type: '' });
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
-      // alert('Enter an item!')
+      showAlert(true, 'Enter an Item!', 'danger')
     }
     else if (name && editing) {
-      //bs
+      setList(list.map((item) => {
+        if (item.id === editID)
+          return { ...item, title: name }
+
+        return item;
+      })
+      )
+      setEditID(null);
+      setEditing(false);
+      setName('');
+      showAlert(true, 'Item updated', 'success');
     }
     else {
       const newItem = { id: new Date().getTime().toString(), title: name }
       setList([...list, newItem]);
+      showAlert(true, 'Item added to the list', 'success')
       setName('');
     }
+  }
+
+  const showAlert = (show = false, msg = '', type = '') => {
+    setAlert({ show, msg, type });
+  }
+
+  const clearList = () => {
+    showAlert(true, 'Empty List', 'danger');
+    setList([]);
+  }
+
+  const removeItem = (id) => {
+    const newList = list.filter((item) => item.id !== id);
+    setList(newList);
+    showAlert(true, 'Item removed successfully', 'danger')
+  }
+
+  const editItem = (id) => {
+    const x = list.find((item) => item.id === id);
+    setEditing(true);
+    setEditID(id);
+    setName(x.title);
   }
 
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>My Grocery List</h3>
         <div className="form-control">
-          <input type='text'
+          <input
+            type='text'
             value={name}
+            className='grocery'
             placeholder="eg: eggs"
             onChange={(e) => setName(e.target.value)}>
           </input>
@@ -43,8 +78,8 @@ function App() {
 
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
-          <button className="clear-btn">Clear Items</button>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className="clear-btn" onClick={clearList} >Clear Items</button>
         </div>
       )}
 
