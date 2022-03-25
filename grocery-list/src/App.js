9d3b629d06b1115d979808b21250ba2cb924069a
LisaVC <lisaviju@gmail.com>
2022-03-25 23:55:42 +0530

    add item functionality added

--- grocery-list/src/App.js
+++ grocery-list/src/App.js
@@ -1,28 +1,53 @@
 import React, { useState } from "react";
-import Alert from 'Alert';
-import List from 'List';
+import Alert from './Alert';
+import List from './List';
 
 function App() {
 
   const [name, setName] = useState('');
-  const [list, setList] = useState();
-  const [edit, setEdit] = useState(false);
+  const [list, setList] = useState([]);
+  const [editing, setEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: true, msg: '', type: '' });
 
-  const handleChange = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+    if (!name) {
+      // alert('Enter an item!')
+    }
+    else if (name && editing) {
+      //bs
+    }
+    else {
+      const newItem = { id: new Date().getTime().toString(), title: name }
+      setList([...list, newItem]);
+      setName('');
+    }
   }
 
   return (
-    <section>
-      <div>
-        <form onSubmit={handleChange(e)}>
-          {alert.show && <Alert />}
-          <input placeholder="eg: eggs"></input>
-          <List />
-        </form>
-      </div>
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>My Grocery List</h3>
+        <div className="form-control">
+          <input type='text'
+            value={name}
+            placeholder="eg: eggs"
+            onChange={(e) => setName(e.target.value)}>
+          </input>
+
+          <button className="submit-btn" type="submit">{editing ? 'edit' : 'submit'}</button>
+        </div>
+      </form>
+
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">Clear Items</button>
+        </div>
+      )}
+
     </section>
   );
 }
